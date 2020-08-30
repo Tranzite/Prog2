6fbe6f06d9f35dde452660b9e2c8a07850b31a1f
Tranzite <54800477+Tranzite@users.noreply.github.com>
2020-08-30 12:37:11 +0200

    homework

--- mata in namn och ålder/mata in namn och ålder/Program.cs
+++ mata in namn och ålder/mata in namn och ålder/Program.cs
@@ -8,29 +8,26 @@ namespace mata_in_namn_och_ålder
         {
 
             Console.WriteLine("Hej detta är mitt program som vill veta vad din ålder och namn är!"); // Detta behövs nog inte förklaras.
-
+            Console.WriteLine();
             Console.WriteLine("Börja gärna med att skriva in ditt namn och sedan ålder");
 
             String name = Console.ReadLine(); // Console.ReadLine är att consolen ska mata in ett värde som i detta tillfället ska vara en string då ditt namn.
-
             Console.WriteLine("Hej " + name + " Hur gammal är du?");
+            Console.WriteLine();
 
             int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Så du är " + age + " år gammal");
+            Console.WriteLine("Så du är alltså " + age + " år gammal");
 
-            bool deadOrNot = true;
-
-            int dead = 100;
-            if (age > dead)
+            if (age > 100)
             {
-                    Console.WriteLine("Ledsen att säga det men du är fan död");
-                
+                Console.WriteLine("du är fan död om du är " + age + " år gammal");
             }
             else
             {
-                Console.WriteLine("Grattis du är inte död än din jävel");
+                Console.WriteLine("Du är ej död grattis!");
             }
+           
             
 
         }
